3d6dce62bac43c0047dd651eb2b8dafcff556a95
Juan Guaba <wava0324@gmail.com>
2018-02-23 19:52:25 -0500

    completando

--- peliculas.py
+++ peliculas.py
@@ -1,72 +1,68 @@
-from arbinor2 import node
-from arbinor2 import arbol
 
 class Cola:
+ 
     def __init__(self):
         self.items=[]
+
     def encolar(self, x):
         self.items.append(x)
+
     def desencolar(self):
         try:
             return self.items.pop(0)
         except IndexError:
-            raise ValueError("La cola está vacía")
+            raise ValueError("La cola esta vacia")
+
     def es_vacia(self):
         return self.items == []
-    def recorrer
 
-    def __init__(self):
-        # inicializa la raiz
-        self.raiz = None
- 
-    def agregarNodo(self, dato):
-        # crea un nuevo nodo y lo devuelve
-        return nodo(dato)
- 
-    def insertar(self, raiz, dato):
-        # inserta un dato nuevo en el árbol
-        if raiz == None:
-            # si no hay nodos en el árbol lo agrega
-            return self.agregarNodo(dato)
-        else:
-            # si hay nodos en el árbol lo recorre
-            if dato <= raiz.dato:
-                # si el dato ingresado es  menor que el dato guardado va al subárbol izquierdo
-                raiz.izq = self.insertar(raiz.izq, dato)
-            else:
-                # si no, procesa el subárbol derecho
-                raiz.der = self.insertar(raiz.der, dato)
-            return raiz
 class Pelicula:
-    nombre=0
-    genero=0
+
+    def __init__(self,nombre,genero):
+        self.nombre=nombre
+        self.genero=genero
 
 class Genero:
-    nombre=0
-    peliculas = Cola()
 
-    def agregarPelicula(self,pelicula):
-        if pelicula.genero==self.nombre:
-            peliculas.encolar(pelicula)
-        else:
-            print ("La pelicula no corresponde a ese genero")
-        
-class Organizar:
-    arbolGeneros = arbol()
-
-    def agregarGenero(genero):
-        arbolGeneros.insert()
+    def __init__(self,nombre):
+        self.nombre = nombre
+        self.items=[]
     
-class GestionAgregar:
-    def _agregarGenero_():
-        
-def main():
-    gaccion = Genero()
-    gaccion.nombre="Accion"
-    pTerminator = Pelicula()
-    pTerminator.genero="Accion"
-    pTerminator.nombre="Termineitor1"
-    org =  Organizar()
-    org.agregarGenero(gaccion) 
+    def encolar(self, x):
+        self.items.append(x)
 
+    def desencolar(self):
+        try:
+            return self.items.pop(0)
+        except IndexError:
+            raise ValueError("La cola esta vacia")
 
+    def es_vacia(self):
+        return self.items == []
+    
+generos = Cola()
+while True:
+    accion = int(input(" 1. Agregar pelicula \n 2. Agregar genero \n 3. Listar \n 4. Salir\n"))
+    print("Accion:",accion)
+    if (accion==4):
+        break
+    elif (accion==2):
+        g1 = Genero(input("Digite el nombre del genero :"))
+        generos.encolar(g1)
+    elif (accion==3):
+        print("HOLA")
+        geT = Cola()
+        while (generos.es_vacia()==False):
+            temp = generos.desencolar()
+            print(temp.nombre)
+            geT.encolar(temp)
+        while (geT.es_vacia()==False):
+            temp = geT.desencolar()
+            generos.encolar(temp)
+    elif (accion==1):
+        geB = input("Ingrese el genero de la pelicula")
+        p1 = Pelicula(input("Ingrese el nombre de la pelicula"),geB)
+        while (generos.es_vacia()==False):
+            temp = generos.desencolar()
+            if(temp.nombre==geB):
+                temp.encolar()
