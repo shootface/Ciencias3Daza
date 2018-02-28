da6c790b74db7628d08bfa3b3385c737785223b9
Juan Guaba <wava0324@gmail.com>
2018-02-28 08:16:02 -0500

    Control

--- CompiladorRudo/compilador.py
+++ CompiladorRudo/compilador.py
@@ -1,3 +1,22 @@
 from pila import Pila
 
-def verificar(cadena)
+def verificarOperadores(cadena):
+    datos=cadena.split(" ")
+    print(datos) 
+    while len(datos)+1!=0:
+        if len(datos)!=0:
+            try:
+                print(int(datos[0]),)
+                datos.pop(0)
+            except ValueError:
+                if datos[0]=="+" or datos[0]=="-" or datos[0]=="*" or datos[0]=="/":
+                    print("todo bien")
+                    datos.pop(0)
+                else:
+                    print("Error en los operadores")
+                    return 0
+        else:
+            print("todo ultra")
+            return 1
+
+print("operador",verificarOperadores("5 7 + 7 8 + 1 4 /"))
